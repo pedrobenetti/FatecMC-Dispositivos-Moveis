a048027e61428d4b0685832e9cb35d4931165ec1
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-03-02 20:33:17 -0300

    Rotas

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,21 +1,27 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
-import { ListUsersController } from "./controllers/ListUsersController";
+
+import { ListUsersController } from "./controllers/ListUserController"
+
+/*
 import { UpdateUserController} from "./controllers/UpdateUserController";
 import { DeleteUserController} from "./controllers/DeleteUserController";
 import {AuthenticateUserController} from "./controllers/AuthenticateUserController";
 import { ensureAdmin} from "./middlewares/ensureAdmin";
 import { ensureAuthenticated} from "./middlewares/ensureAuthenticated";
 
-const createUserController  = new CreateUserController();
-const listUsersController  = new ListUsersController();
+
+
 const updateUserController  = new UpdateUserController();
 const deleteUserController  = new DeleteUserController();
 const autenticationUserController  = new AuthenticateUserController();
-
+*/
+const createUserController  = new CreateUserController();
+const listUsersController  = new ListUsersController();
 const router = Router();
-
 router.post("/users", createUserController.handle);
+router.get("/listusers", listUsersController.handle);
+/*
 router.post("/login", autenticationUserController.handle);
 
 router.get("/users",  ensureAuthenticated,ensureAdmin,listUsersController.handle);
@@ -23,5 +29,5 @@ router.get("/users",  ensureAuthenticated,ensureAdmin,listUsersController.handle
 router.use(ensureAdmin)
 router.delete("/users/:id", deleteUserController.handle);
 router.put("/users", updateUserController.handle);
-
+*/
 export {router}
