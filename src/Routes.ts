cd1ef22d4d303c1d969d0fea0852925100f8696c
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-03-02 19:53:22 -0300

    Configurações de rotas

---
+++ src/Routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { ListUsersController } from "./controllers/ListUsersController";
+import { UpdateUserController} from "./controllers/UpdateUserController";
+import { DeleteUserController} from "./controllers/DeleteUserController";
+import {AuthenticateUserController} from "./controllers/AuthenticateUserController";
+import { ensureAdmin} from "./middlewares/ensureAdmin";
+import { ensureAuthenticated} from "./middlewares/ensureAuthenticated";
+
+const createUserController  = new CreateUserController();
+const listUsersController  = new ListUsersController();
+const updateUserController  = new UpdateUserController();
+const deleteUserController  = new DeleteUserController();
+const autenticationUserController  = new AuthenticateUserController();
+
+const router = Router();
+
+router.post("/users", createUserController.handle);
+router.post("/login", autenticationUserController.handle);
+
+router.get("/users",  ensureAuthenticated,ensureAdmin,listUsersController.handle);
+
+router.use(ensureAdmin)
+router.delete("/users/:id", deleteUserController.handle);
+router.put("/users", updateUserController.handle);
+
+export {router}
