d192c1a36ff50014d2ad0a305ea4bd70ef0ad13b
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-03-02 20:36:16 -0300

    Controller para listagem de usuários

--- src/controllers/ListUserController.ts
+++ src/controllers/ListUserController.ts
@@ -1,12 +1,26 @@
 import { Request, Response } from "express";
-import { ListUserService } from "../services/ListUsersService";
+//import { ListUserService } from "../services/ListUsersService";
 
 class ListUsersController {
   async handle(request: Request, response: Response) {
-    const listUsersService = new ListUserService();
-
-    const users = await listUsersService.execute();
-
+  
+    const users = [ 
+        {
+        
+            name:"Fabio",
+            email:"fabio@gmail.com",
+            admin:false,
+            password:"1234"             
+      },
+      {
+        
+        name:"Marcio",
+        email:"marcio@gmail.com",
+        admin:false,
+        password:"1234"             
+     }
+    
+    ];
     return response.json(users);
   }
 }
