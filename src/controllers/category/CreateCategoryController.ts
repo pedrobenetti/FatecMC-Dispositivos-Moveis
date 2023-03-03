195d295e58d29bb36523997e8fd98e3504e024e8
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-03-03 12:39:58 -0300

    Controller para criação de categorias

---
+++ src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+class CreateCategoryController { 
+
+    async handle(request: Request, response: Response) {
+        const { id, name } = request.body;
+
+        console.log(id)
+        console.log(name)
+
+        const category = {
+            id:id,
+            name:name
+        };
+
+        return response.json(category);
+    }
+}
+
+export { CreateCategoryController };
