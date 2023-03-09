efaeb0acbb80f5a27be01ec8f8fae1a4dd430df3
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-03-09 00:55:09 -0300

    Controller para atualização de usuarios

---
+++ src/controllers/user/UptadeUserController.ts
@@ -0,0 +1,26 @@
+import { request, Request, Response } from "express";
+
+class UpdateUserController {
+
+    async handle(request: Request, response: Response){
+        const { id, name, email, admin, password } = request.body;
+
+        console.log(id);
+        console.log(name);
+        console.log(email);
+        console.log(admin);
+        console.log(password);
+
+        const user = {
+            name:name,
+            email:email,
+            admin:admin,
+            password:password
+        };
+
+        return response.json(user);
+    }
+}
+
+
+export { UpdateUserController };
