1a5eab20921e3c509888756fe2b74d90bcceb1e6
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-03-09 00:55:34 -0300

    Configuração de rotas

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { ListUsersController } from "./controllers/user/ListUserController"
+import { DeleteUserController } from "./controllers/user/DeleteUserController";
+import { UpdateUserController } from "./controllers/user/UptadeUserController";
 
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
@@ -12,36 +15,49 @@ import { CreateSaleController } from "./controllers/sales/CreateSalesController"
 import { ListSalesController } from "./controllers/sales/ListSalesController";
 
 /*
-import { UpdateUserController} from "./controllers/UpdateUserController";
-import { DeleteUserController} from "./controllers/DeleteUserController";
 import {AuthenticateUserController} from "./controllers/AuthenticateUserController";
 import { ensureAdmin} from "./middlewares/ensureAdmin";
 import { ensureAuthenticated} from "./middlewares/ensureAuthenticated";
 
 
 
-const updateUserController  = new UpdateUserController();
-const deleteUserController  = new DeleteUserController();
 const autenticationUserController  = new AuthenticateUserController();
 */
+
+
+
 const createUserController  = new CreateUserController();
-const listUsersController  = new ListUsersController();
+const listUsersController   = new ListUsersController();
+const updateUserController  = new UpdateUserController();
+const deleteUserController  = new DeleteUserController();
 const router = Router();
+
 router.post("/users", createUserController.handle);
-router.get("/listusers", listUsersController.handle);
+router.get("/users", listUsersController.handle);
+router.put("/users", updateUserController.handle);
+router.delete("/users/:id", deleteUserController.handle);
+
+
 
 const createCategoryController = new CreateCategoryController();
 const listCategoryController = new ListCategoryController();
+
 router.post("/category", createCategoryController.handle);
 router.get("/listcategory", listCategoryController.handle);
 
+
+
 const createProductController = new CreateProductController();
 const listProductController = new ListProductController();
+
 router.post("/product", createProductController.handle);
 router.get("/listproduct", listProductController.handle);
 
+
+
 const createSalesController = new CreateSaleController();
 const listSalesController = new ListSalesController();
+
 router.post("/sales", createSalesController.handle);
 router.get("/listsales", listSalesController.handle);
 
