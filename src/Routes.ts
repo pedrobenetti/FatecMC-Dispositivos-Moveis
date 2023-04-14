1b46af8a295930e815b79cd9f877804b6aaefd3d
Pedro Benetti <pedrobenetti@outlook.com.br>
2023-04-13 21:52:41 -0300

    Routes Config

--- src/Routes.ts
+++ src/Routes.ts
@@ -14,26 +14,33 @@ import { ListProductController } from "./controllers/product/ListProductControll
 import { CreateSaleController } from "./controllers/sales/CreateSalesController";
 import { ListSalesController } from "./controllers/sales/ListSalesController";
 
+
+import { AuthenticateUserController } from "./controllers/user/AuthenticateUserController";
+
+import { ensureAuthenticated } from "./middleware/EnsureAuthenticated";
+
 /*
-import {AuthenticateUserController} from "./controllers/AuthenticateUserController";
 import { ensureAdmin} from "./middlewares/ensureAdmin";
-import { ensureAuthenticated} from "./middlewares/ensureAuthenticated";
 
 
 
-const autenticationUserController  = new AuthenticateUserController();
 */
-
 
 
 const createUserController  = new CreateUserController();
 const listUsersController   = new ListUsersController();
 const updateUserController  = new UpdateUserController();
 const deleteUserController  = new DeleteUserController();
+const autenticationUserController  = new AuthenticateUserController();
+
 const router = Router();
 
+router.post("/login", autenticationUserController.handle);
+router.get("/listusers", listUsersController.handle);
+
+router.use(ensureAuthenticated);
+
 router.post("/users", createUserController.handle);
-router.get("/users", listUsersController.handle);
 router.put("/users", updateUserController.handle);
 router.delete("/users/:id", deleteUserController.handle);
 
@@ -63,7 +70,6 @@ router.get("/listsales", listSalesController.handle);
 
 
 /*
-router.post("/login", autenticationUserController.handle);
 
 router.get("/users",  ensureAuthenticated,ensureAdmin,listUsersController.handle);
 
